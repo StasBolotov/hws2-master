cfe4bc926d396ea2f54db109e899059f4e8e9461
StasBolotov <Stashek.web.404@gmail.com>
2026-04-21 10:13:49 +0300

    ДЗ-12

--- src/s2-homeworks/hw10/bll/store.ts
+++ src/s2-homeworks/hw10/bll/store.ts
@@ -1,19 +1,20 @@
 import { combineReducers, legacy_createStore } from 'redux'
 import { loadingReducer } from './loadingReducer'
+import { themeReducer } from '../../hw12/bll/themeReducer' // 1. Добавь импорт
 
 const rootReducer = combineReducers({
-    loading: loadingReducer, // название поля 'loading' важно для useSelector в HW10.tsx
+    loading: loadingReducer, // Сохраняем для HW10
+    theme: themeReducer,     // 2. Добавляем для HW12
 })
 
 const store = legacy_createStore(rootReducer)
 
 export default store
 
-// Автоматическое определение типа всего состояния (AppStoreType)
+// Автоматически обновит тип, теперь AppStoreType будет содержать и loading, и theme
 export type AppStoreType = ReturnType<typeof rootReducer>
 
-// Тип для диспатча (пригодится для типизации хуков в будущем)
 export type AppDispatch = typeof store.dispatch
 
 // @ts-ignore
-window.store = store // для возможности обращения к стору в консоли браузера
+window.store = store
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,21 @@
-const initState = {
+const initialState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+type StateType = typeof initialState
 
+export const themeReducer = (state: StateType = initialState, action: ActionType): StateType => {
+    switch (action.type) {
+        case 'SET_THEME_ID':
+            return {
+                ...state,
+                themeId: action.id
+            }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ActionType = ReturnType<typeof changeThemeId>
+
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id } as const)
